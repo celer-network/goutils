c1d2ddf4c2fb5a9fd20f17823e8c82cdcd248ffc
Junda Liu <junda@celer.network>
2021-11-01 17:37:57 -0700

    done

--- eth/awskms.go
+++ eth/awskms.go
@@ -1,13 +1,18 @@
 package eth
 
 import (
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"fmt"
 	"math/big"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
@@ -16,6 +21,7 @@ type KmsSigner struct {
 	chainId  *big.Int
 	keyAlias *string
 	kms      *kms.KMS
+	Addr     common.Address
 }
 
 func NewKmsSigner(region, keyAlias, awsKey, awsSec string, chainId *big.Int) (*KmsSigner, error) {
@@ -24,17 +30,33 @@ func NewKmsSigner(region, keyAlias, awsKey, awsSec string, chainId *big.Int) (*K
 		Region:      aws.String(region)},
 	)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("NewSession err: %w", err)
 	}
+	svc := kms.New(sess)
+	resp, err := svc.GetPublicKey(&kms.GetPublicKeyInput{
+		KeyId: aws.String(keyAlias),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("GetPublicKey err: %w", err)
+	}
+	pub := new(PubKeyAsn)
+	_, err = asn1.Unmarshal(resp.PublicKey, pub)
+	if err != nil {
+		return nil, fmt.Errorf("asn1.Unmarshal err: %w, resp.PublicKey: %x", err, resp.PublicKey)
+	}
+	// skip first byte as it's just an indicator whether compress, and use last 20 bytes of hash
+	// see PubkeyToAddress https://github.com/ethereum/go-ethereum/blob/master/crypto/crypto.go#L276
+	addr := common.BytesToAddress(crypto.Keccak256(pub.PubKey.Bytes[1:])[12:])
 	return &KmsSigner{
 		chainId:  new(big.Int).Set(chainId),
 		keyAlias: aws.String(keyAlias),
-		kms:      kms.New(sess),
+		kms:      svc,
+		Addr:     addr,
 	}, nil
 }
 
 func (s *KmsSigner) SignEthMessage(data []byte) ([]byte, error) {
-	return nil, nil
+	return s.Sign(GeneratePrefixedHash(data))
 }
 
 func (s *KmsSigner) SignEthTransaction(rawTx []byte) ([]byte, error) {
@@ -54,6 +76,62 @@ func (s *KmsSigner) SignEthTransaction(rawTx []byte) ([]byte, error) {
 	return rlp.EncodeToBytes(tx)
 }
 
-func (s *KmsSigner) Sign(raw []byte) ([]byte, error) {
-	return nil, nil
+// input must be hash, return 65 bytes sig or error. sig[64] is 0 or 1
+func (s *KmsSigner) Sign(hash []byte) ([]byte, error) {
+	sig, err := s.kms.Sign(&kms.SignInput{
+		KeyId:            s.keyAlias,
+		MessageType:      aws.String("DIGEST"),
+		Message:          hash,
+		SigningAlgorithm: aws.String("ECDSA_SHA_256"),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Sign err: %w", err)
+	}
+	rs := new(RS)
+	_, err = asn1.Unmarshal(sig.Signature, rs)
+	if err != nil {
+		return nil, fmt.Errorf("asn1.Unmarshal err: %w, sig.Signature: %x", err, sig.Signature)
+	}
+	retSig := make([]byte, 65)
+	copy(retSig, padBigInt(rs.R))
+	copy(retSig[32:], padBigInt(rs.S))
+	// now try recover to see if v is 0 or 1
+	pubKey, err := crypto.SigToPub(hash, retSig)
+	if err != nil {
+		return nil, fmt.Errorf("SigToPub: %w, hash: %x, retSig: %x", err, hash, retSig)
+	}
+	addr := crypto.PubkeyToAddress(*pubKey)
+	if addr == s.Addr {
+		return retSig, nil
+	}
+	retSig[64] = 1
+	pubKey, err = crypto.SigToPub(hash, retSig)
+	if err != nil {
+		return nil, fmt.Errorf("SigToPub: %w, hash: %x, retSig: %x", err, hash, retSig)
+	}
+	addr = crypto.PubkeyToAddress(*pubKey)
+	if addr == s.Addr {
+		return retSig, nil
+	}
+	return nil, fmt.Errorf("recovered addr %x not match expected %x", addr, s.Addr)
+}
+
+type PubKeyAsn struct {
+	Algo   pkix.AlgorithmIdentifier
+	PubKey asn1.BitString
+}
+
+type RS struct {
+	R, S *big.Int
+}
+
+// guarantee return len([]byte) = 32
+func padBigInt(i *big.Int) []byte {
+	ret := i.Bytes()
+	if len(ret) == 32 {
+		return ret
+	}
+	ret2 := make([]byte, 32)
+	copy(ret2[32-len(ret):], ret)
+	return ret2
 }
